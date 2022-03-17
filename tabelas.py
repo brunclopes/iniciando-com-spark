612921eff4bdbeaee0383d9522e7e7fd76b60184
Bruno Lopes <86599110+brunclopes@users.noreply.github.com>
2022-03-16 22:31:57 -0300

    Add files via upload
    Comitando arquivos do curso realizado na Udemy, do instrutor Fernando Amaral.

---
+++ tabelas.py
@@ -0,0 +1,35 @@
+# faça você mesmo - teste
+
+#criando o banco de dados 
+# sudo -u postgres psql
+#create database tabelas
+
+#conectando ao banco de dados
+# \c tabelas;
+
+# criando aplicação
+
+import sys. getopt
+from pyspark.sql import SparkSession
+
+if __name__=="__main__":
+	spark = SparkSession.builder.appName("Tabelas").getOrCreate()
+	opts, args = getopt.getopt(sys.argv[1:], "a:t:")
+	arquivo, tabela = "",""
+	for opt, arg in opts:
+		if opt== "-a":
+			arquivo = arg
+		elif opt == "-t":
+			tabela = arg
+	
+	#lendo o arquivo 
+	df = spark.read.load(arquivo)
+	df.write.format("jdbc").option("url","jdbc:postgresql://localhost:5432/-tabelas").option("dbtable",tabela).option("user","postgres").option("password","19941994").option("driver","org.postgresql.Driver")
+	
+	spark.stop()
+	
+
+# chamando o arquivo: spark-submit --jars /home/bruno/postgresql-42.3.3.jar tabelas.py -a /home/bruno/download/Atividades/Vendas.parquet -t Vendas 
+
+# verificando no banco 
+# select * from vendas
